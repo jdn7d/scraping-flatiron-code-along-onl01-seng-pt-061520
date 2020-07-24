74b6e44620c00e5ddc8ae39d0ad19d6550640f23
Jessie Donovan <donovanajessie@gmail.com>
2020-07-24 13:02:49 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,9 +2,7 @@ class Course
   attr_accessor :title, :schedule, :description
   @@all = []
   def initialize()
-    @title = title
-    @schedule = schedule
-    @description = description
+    
     @@all << self
   end
 
