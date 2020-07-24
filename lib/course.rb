cf4cb7852d4d1b25e148bd19fea22412b7a7d85a
Jessie Donovan <donovanajessie@gmail.com>
2020-07-24 13:01:22 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,8 @@
 class Course
-
+  attr_accessor :title, :schedule, :description
+  def initialize(title, schedule, description)
+    @title = title
+    @schedule = schedule
+    @description = description
+  end
 end
