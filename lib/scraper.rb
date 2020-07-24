6a2911e46252fcffbfaffaabb3974f8cddefae71
Jessie Donovan <donovanajessie@gmail.com>
2020-07-24 13:11:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
-
+require 'pry'
 require_relative './course.rb'
 
 class Scraper
+  def self.get_page
+    doc = Nokogiri::HTML(open("http://learn-
+    co-curriculum.github.io/site-for-scraping/courses"))
+  end
+  binding.pry
   
   def print_courses
     self.make_courses
